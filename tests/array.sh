a45cb6072c29540791b26feb3a641d1162b66817
AndriiKostromytskyi <53262341+AndriiKostromytskyi@users.noreply.github.com>
2022-07-28 09:18:58 +0300

    Update array.sh

--- tests/array.sh
+++ tests/array.sh
@@ -1,3 +1,6 @@
-for n in [list]; do
-    mkdir 
+#!/bin/bash
+Letters=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+for (( var = 0; var < $1; var++ )) do
+    name="folder_${Letters[$var]}"
+    mkdir $name
 done
